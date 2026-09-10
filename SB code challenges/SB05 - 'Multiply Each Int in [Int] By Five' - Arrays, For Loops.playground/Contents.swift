b137f38cf48e3a66c980e3af9c3818e77cc3a993
Ryker <rykerfarabee07@icloud.com>
2026-09-10 13:31:12 -0600

    no message

---
+++ SB code challenges/SB05 - 'Multiply Each Int in [Int] By Five' - Arrays, For Loops.playground/Contents.swift
@@ -0,0 +1,12 @@
+//  🏔️ MTECH Code Challenge SB05: "Multiply Each Int in [Int] By Five"
+//  Concept: Practice looping over arrays
+
+//  Instructions:
+    //  Create a function that takes in an array of Int's as a parameter
+    //  You will need to multiply each Int in the array by 5.
+    //  The function will return the array of new Ints.
+
+//  ⌺ Black Diamond Challenge:
+    //  Create a second function for the same purpose. Return an array of Ints without using a for loop. (This will require a special method -- look it up!)
+
+import Foundation
---
+++ SwiftUIExcercises/SwiftUIExcercises/SwiftUIExcercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExcercisesApp.swift
+//  SwiftUIExcercises
+//
+//  Created by Ryker Farabee on 9/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExcercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomScreen()
+        }
+    }
+}
